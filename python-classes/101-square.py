700f0984b7a15337a449f9123427b3eee5226bf7
owizdom <gokun4621@example.com>
2025-07-05 08:16:00 +0200

    u

---
+++ python-classes/101-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Defines a square with printable representation."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not isinstance(value[0], int) or
+                not isinstance(value[1], int) or
+                value[0] < 0 or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for _ in range(self.__position[1]):
+            print()
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        if self.__size == 0:
+            return ""
+        lines = []
+        for _ in range(self.__position[1]):
+            lines.append("")
+        for _ in range(self.__size):
+            lines.append(" " * self.__position[0] + "#" * self.__size)
+        return "\n".join(lines)
