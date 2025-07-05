205c7a32fdc5fb880db9d8aaa681a1830056048b
owizdom <gokun4621@example.com>
2025-07-05 08:16:00 +0200

    u

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Defines a LockedClass that only allows 'first_name' as an attribute."""
-
-
+"""Defines LockedClass that only allows 'first_name' as an attribute."""
 class LockedClass:
-    """LockedClass prevents dynamic creation of attributes except 'first_name'."""
-    __slots__ = ("first_name",)
+    """Prevents new attributes except 'first_name'."""
+    __slots__ = ('first_name',)
