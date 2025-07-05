af31fd8d49e649d3702842937654213ab3a0f049
owizdom <gokun4621@example.com>
2025-07-05 08:16:00 +0200

    u

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
-def magic_string(_magic=[0]):
-    _magic[0] += 1
-    return ', '.join(['BestSchool'] * _magic[0])
+def magic_string(lst=[]):
+    lst.append("BestSchool")
+    return ", ".join(lst)
